9cfe3fbac531e1ef0a479e689bdddbf9abf13766
Ruslan <ep8kha@gmail.com>
2019-08-03 18:21:01 +0500

    hwag_init

--- source/het.c
+++ source/het.c
@@ -1,10 +1,8 @@
 #include "het.h"
 
-void hwag_init(hetBASE_t* het,uint8_t pin,bool arst,bool ted,bool cri,bool fil) {
-    het->HWAGCR0 = 0;
+void hwag_init(hetBASE_t* het,uint8_t pin,bool arst,bool ted,bool cri,bool fil,uint8_t stwd,uint8_t thnb,uint16_t fil1,uint16_t fil2) {
     het->HWAGCR0 = HWAGCR0_RESET; //HWAG Enable;
 
-    het->HWAPINSEL = 0;
     het->HWAPINSEL = HWAPINSEL_PINSEL(pin); //HWAG Pin Select
 
     het->HWAGCR2 = 0;
@@ -12,6 +10,14 @@ void hwag_init(hetBASE_t* het,uint8_t pin,bool arst,bool ted,bool cri,bool fil) 
     if(ted) het->HWAGCR2 |= HWAGCR2_TED;
     if(cri) het->HWAGCR2 |= HWAGCR2_CRI;
     if(fil) het->HWAGCR2 |= HWAGCR2_FIL;
+
+    het->HWASTWD = HWASTWD_STWD(stwd);
+
+    het->HWATHNB = HWATHNB_THNB(thnb);
+
+    het->HWAFIL = HWAFIL_FIL1(fil1);
+
+    het->HWAFIL2 = HWAFIL2_FIL2(fil2);
 }
 
 void hwag_start(hetBASE_t* het) {
@@ -39,22 +45,6 @@ bool hwag_interrupt_flag_read(hetBASE_t* het,uint32_t hwena) {
 
 void hwag_interrupt_flag_clear(hetBASE_t* het,uint32_t hwena) {
     het->HWAFLG = hwena;
-}
-
-void hwag_step_width(hetBASE_t* het,uint8_t stwd) {
-    het->HWASTWD = HWASTWD_STWD(stwd);
-}
-
-void hwag_teeth_number(hetBASE_t* het,uint8_t thnb) {
-    het->HWATHNB = HWATHNB_THNB(thnb);
-}
-
-void hwag_filter_1(hetBASE_t* het,uint16_t fil1) {
-    het->HWAFIL = HWAFIL_FIL1(fil1);
-}
-
-void hwag_filter_2(hetBASE_t* het,uint16_t fil2) {
-    het->HWAFIL2 = HWAFIL2_FIL2(fil2);
 }
 
 
