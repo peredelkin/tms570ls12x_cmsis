ddb825dbe3173da80ab4b3959ffcc0769b8dd8c2
Ruslan <ep8kha@gmail.com>
2019-08-03 18:00:11 +0500

    het.h het.c

--- include/het.h
+++ include/het.h
@@ -111,67 +111,67 @@
 
 typedef volatile struct hetBase
 {
-    /*00*/ uint32_t HETGCR;        /*Global Configuration Register*/
-    /*04*/ uint32_t HETPFR;        /*Prescale Factor Register*/
-    /*08*/ uint32_t HETADDR;       /*NHET Current Address Register*/
-    /*0C*/ uint32_t HETOFF1;       /*Offset Index Priority Level 1 Register*/
-    /*10*/ uint32_t HETOFF2;       /*Offset Index Priority Level 2 Register*/
-    /*14*/ uint32_t HETINTENAS;    /*Interrupt Enable Set Register*/
-    /*18*/ uint32_t HETINTENAC;    /*Interrupt Enable Clear Register*/
-    /*1C*/ uint32_t HETEXC1;       /*Exception Control Register 1*/
-    /*20*/ uint32_t HETEXC2;       /*Exception Control Register 2*/
-    /*24*/ uint32_t HETPRY;        /*Interrupt Priority Register*/
-    /*28*/ uint32_t HETFLG;        /*Interrupt Flag Register*/
-    /*2C*/ uint32_t HETAND;        /*AND Share Control Register*/
+    /*00*/ uint32_t HETGCR;        /** Global Configuration Register */
+    /*04*/ uint32_t HETPFR;        /** Prescale Factor Register */
+    /*08*/ uint32_t HETADDR;       /** NHET Current Address Register */
+    /*0C*/ uint32_t HETOFF1;       /** Offset Index Priority Level 1 Register */
+    /*10*/ uint32_t HETOFF2;       /** Offset Index Priority Level 2 Register */
+    /*14*/ uint32_t HETINTENAS;    /** Interrupt Enable Set Register */
+    /*18*/ uint32_t HETINTENAC;    /** Interrupt Enable Clear Register */
+    /*1C*/ uint32_t HETEXC1;       /** Exception Control Register 1 */
+    /*20*/ uint32_t HETEXC2;       /** Exception Control Register 2 */
+    /*24*/ uint32_t HETPRY;        /** Interrupt Priority Register */
+    /*28*/ uint32_t HETFLG;        /** Interrupt Flag Register */
+    /*2C*/ uint32_t HETAND;        /** AND Share Control Register */
     /*30*/ uint32_t : 32U;
-    /*34*/ uint32_t HETHRSH;       /*HR Share Control Register*/
-    /*38*/ uint32_t HETXOR;        /*HR XOR-Share Control Register*/
-    /*3C*/ uint32_t HETREQENS;     /*Request Enable Set Register*/
-    /*40*/ uint32_t HETREQENC;     /*Request Enable Clear Register*/
-    /*44*/ uint32_t HETREQDS;      /*Request Destination Select Register*/
+    /*34*/ uint32_t HETHRSH;       /** HR Share Control Register */
+    /*38*/ uint32_t HETXOR;        /** HR XOR-Share Control Register */
+    /*3C*/ uint32_t HETREQENS;     /** Request Enable Set Register */
+    /*40*/ uint32_t HETREQENC;     /** Request Enable Clear Register */
+    /*44*/ uint32_t HETREQDS;      /** Request Destination Select Register */
     /*48*/ uint32_t : 32U;
-    /*4C*/ uint32_t HETDIR;        /*NHET Direction Register*/
-    /*50*/ uint32_t HETDIN;        /*NHET Data Input Register*/
-    /*54*/ uint32_t HETDOUT;       /*NHET Data Output Register*/
-    /*58*/ uint32_t HETDSET;       /*NHET Data Set Register*/
-    /*5C*/ uint32_t HETDCLR;       /*NHET Data Clear Register*/
-    /*60*/ uint32_t HETPDR;        /*NHET Open Drain Register*/
-    /*64*/ uint32_t HETPULDIS;     /*NHET Pull Disable Register*/
-    /*68*/ uint32_t HETPSL;        /*NHET Pull Select Register*/
+    /*4C*/ uint32_t HETDIR;        /** NHET Direction Register */
+    /*50*/ uint32_t HETDIN;        /** NHET Data Input Register */
+    /*54*/ uint32_t HETDOUT;       /** NHET Data Output Register */
+    /*58*/ uint32_t HETDSET;       /** NHET Data Set Register */
+    /*5C*/ uint32_t HETDCLR;       /** NHET Data Clear Register */
+    /*60*/ uint32_t HETPDR;        /** NHET Open Drain Register */
+    /*64*/ uint32_t HETPULDIS;     /** NHET Pull Disable Register */
+    /*68*/ uint32_t HETPSL;        /** NHET Pull Select Register */
     /*6C*/ uint32_t : 32U;
     /*70*/ uint32_t : 32U;
-    /*74*/ uint32_t HETPCR;        /*Parity Control Register*/
-    /*78*/ uint32_t HETPAR;        /*Parity Address Register*/
-    /*7C*/ uint32_t HETPPR;        /*Parity Pin Register*/
-    /*80*/ uint32_t HETSFPRLD;     /*Suppression Filter Preload Register*/
-    /*84*/ uint32_t HETSFENA;      /*Suppression Filter Enable Register*/
+    /*74*/ uint32_t HETPCR;        /** Parity Control Register */
+    /*78*/ uint32_t HETPAR;        /** Parity Address Register */
+    /*7C*/ uint32_t HETPPR;        /** Parity Pin Register */
+    /*80*/ uint32_t HETSFPRLD;     /** Suppression Filter Preload Register */
+    /*84*/ uint32_t HETSFENA;      /** Suppression Filter Enable Register */
     /*88*/ uint32_t : 32U;
-    /*8C*/ uint32_t HETLBPSEL;     /*Loop Back Pair Select Register*/
-    /*90*/ uint32_t HETLBPDIR;     /*Loop Back Pair Direction Register*/
-    /*94*/ uint32_t HETPINDIS;     /*NHET Pin Disable Register*/
+    /*8C*/ uint32_t HETLBPSEL;     /** Loop Back Pair Select Register */
+    /*90*/ uint32_t HETLBPDIR;     /** Loop Back Pair Direction Register */
+    /*94*/ uint32_t HETPINDIS;     /** NHET Pin Disable Register */
     /*98*/ uint32_t : 32U;
-    /*9C*/ uint32_t HWAPINSEL;     /*HWAG Pin Select Register*/
-    /*A0*/ uint32_t HWAGCR0;       /*HWAG Global Control Register 0*/
-    /*A4*/ uint32_t HWAGCR1;       /*HWAG Global Control Register 1*/
-    /*A8*/ uint32_t HWAGCR2;       /*HWAG Global Control Register 2*/
-    /*AC*/ uint32_t HWAENASET;     /*HWAG Interrupt Enable Set Register*/
-    /*B0*/ uint32_t HWAENACLR;     /*HWAG Interrupt Enable Clear Register*/
-    /*B4*/ uint32_t HWALVLSET;     /*HWAG Interrupt Level Set Register*/
-    /*B8*/ uint32_t HWALVLCLR;     /*HWAG Interrupt Level Clear Register*/
-    /*BC*/ uint32_t HWAFLG;        /*HWAG Interrupt Flag Register*/
-    /*C0*/ uint32_t HWAOFF0;       /*HWAG Interrupt Offset Register 1*/
-    /*C4*/ uint32_t HWAOFF1;       /*HWAG Interrupt Offset Register 2*/
-    /*C8*/ uint32_t HWAACNT;       /*HWAG Angle Value Register*/
-    /*CC*/ uint32_t HWAPCNT1;      /*HWAG Previous Tooth Period Value Register*/
-    /*D0*/ uint32_t HWAPCNT;       /*HWAG Current Tooth Period Value Register*/
-    /*D4*/ uint32_t HWASTWD;       /*HWAG Step Width Register*/
-    /*D8*/ uint32_t HWATHNB;       /*HWAG Teeth Number Register*/
-    /*DC*/ uint32_t HWATHVL;       /*HWAG Current Teeth Number Register*/
-    /*E0*/ uint32_t HWAFIL;        /*HWAG Filter Register*/
+    /*9C*/ uint32_t HWAPINSEL;     /** HWAG Pin Select Register */
+    /*A0*/ uint32_t HWAGCR0;       /** HWAG Global Control Register 0 */
+    /*A4*/ uint32_t HWAGCR1;       /** HWAG Global Control Register 1 */
+    /*A8*/ uint32_t HWAGCR2;       /** HWAG Global Control Register 2 */
+    /*AC*/ uint32_t HWAENASET;     /** HWAG Interrupt Enable Set Register */
+    /*B0*/ uint32_t HWAENACLR;     /** HWAG Interrupt Enable Clear Register */
+    /*B4*/ uint32_t HWALVLSET;     /** HWAG Interrupt Level Set Register */
+    /*B8*/ uint32_t HWALVLCLR;     /** HWAG Interrupt Level Clear Register */
+    /*BC*/ uint32_t HWAFLG;        /** HWAG Interrupt Flag Register */
+    /*C0*/ uint32_t HWAOFF0;       /** HWAG Interrupt Offset Register 1 */
+    /*C4*/ uint32_t HWAOFF1;       /** HWAG Interrupt Offset Register 2 */
+    /*C8*/ uint32_t HWAACNT;       /** HWAG Angle Value Register */
+    /*CC*/ uint32_t HWAPCNT1;      /** HWAG Previous Tooth Period Value Register */
+    /*D0*/ uint32_t HWAPCNT;       /** HWAG Current Tooth Period Value Register */
+    /*D4*/ uint32_t HWASTWD;       /** HWAG Step Width Register */
+    /*D8*/ uint32_t HWATHNB;       /** HWAG Teeth Number Register */
+    /*DC*/ uint32_t HWATHVL;       /** HWAG Current Teeth Number Register */
+    /*E0*/ uint32_t HWAFIL;        /** HWAG Filter Register */
     /*E4*/ uint32_t : 32U;
-    /*E8*/ uint32_t HWAFIL2;       /*HWAG Filter Register 2*/
+    /*E8*/ uint32_t HWAFIL2;       /** HWAG Filter Register 2 */
     /*EC*/ uint32_t : 32U;
-    /*F0*/ uint32_t HWAANGI;       /*HWAG Angle Increment Register*/
+    /*F0*/ uint32_t HWAANGI;       /** HWAG Angle Increment Register */
 
 } hetBASE_t;
 
--- source/het.c
+++ source/het.c
@@ -1,2 +1,66 @@
 #include "het.h"
 
+void hwag_init(hetBASE_t* het,uint8_t pin,bool arst,bool ted,bool cri,bool fil) {
+    het->HWAGCR0 = 0;
+    het->HWAGCR0 = HWAGCR0_RESET; //HWAG Enable;
+
+    het->HWAPINSEL = 0;
+    het->HWAPINSEL = HWAPINSEL_PINSEL(pin); //HWAG Pin Select
+
+    het->HWAGCR2 = 0;
+    if(arst)het->HWAGCR2 |= HWAGCR2_ARST;
+    if(ted) het->HWAGCR2 |= HWAGCR2_TED;
+    if(cri) het->HWAGCR2 |= HWAGCR2_CRI;
+    if(fil) het->HWAGCR2 |= HWAGCR2_FIL;
+}
+
+void hwag_start(hetBASE_t* het) {
+    het->HWAGCR2 |= HWAGCR2_STRT;
+}
+
+void hwag_stop(hetBASE_t* het) {
+    het->HWAGCR2 &= ~HWAGCR2_STRT;
+}
+
+void hwag_interrupt(hetBASE_t* het,uint32_t hwena,bool ena) {
+    if(ena) het->HWAENASET = hwena;
+    else het->HWAENACLR = hwena;
+}
+
+void hwag_interrupt_lvl(hetBASE_t* het,uint32_t hwena,bool lvl) {
+    if(lvl) het->HWALVLSET = hwena;
+    else het->HWALVLCLR = hwena;
+}
+
+bool hwag_interrupt_flag_read(hetBASE_t* het,uint32_t hwena) {
+    if((het->HWAFLG) & hwena) return true;
+    else return false;
+}
+
+void hwag_interrupt_flag_clear(hetBASE_t* het,uint32_t hwena) {
+    het->HWAFLG = hwena;
+}
+
+void hwag_step_width(hetBASE_t* het,uint8_t stwd) {
+    het->HWASTWD = HWASTWD_STWD(stwd);
+}
+
+void hwag_teeth_number(hetBASE_t* het,uint8_t thnb) {
+    het->HWATHNB = HWATHNB_THNB(thnb);
+}
+
+void hwag_filter_1(hetBASE_t* het,uint16_t fil1) {
+    het->HWAFIL = HWAFIL_FIL1(fil1);
+}
+
+void hwag_filter_2(hetBASE_t* het,uint16_t fil2) {
+    het->HWAFIL2 = HWAFIL2_FIL2(fil2);
+}
+
+
+
+
+
+
+
+
